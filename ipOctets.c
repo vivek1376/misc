107354f526a1eb04ab55a630942b44d434d589a6
Vivek Mehra <vivek.1376@gmail.com>
2015-01-02 23:32:50 +0530

    wrote program to break IP address string into octets

---
+++ ipOctets.c
@@ -0,0 +1,40 @@
+/* breaks IP addr string into octets <vivek(at)gmail.com> */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define _P2I(p) (unsigned long int)(p)
+
+char* splitOctet(char *, char **);
+
+int main()
+{
+    char ip[]="192.168.1.132";
+    char *p=ip, **octet=(char **)malloc(4*sizeof(char *));
+
+    int i=0;
+    while((p=splitOctet(p, &octet[i]))!=NULL)
+	i++;
+
+    for(i=0;i<4;i++)
+	printf("%s\n",octet[i]);
+
+    return 0;
+}
+
+char* splitOctet(char *ipStr, char **octet)
+{
+    char *p=ipStr;
+
+    while(*p!='.' && *p!='\0')
+	p++;
+    
+    unsigned long int count=_P2I(p)-_P2I(ipStr);
+
+    *octet=(char *)malloc((count+1)*sizeof(char));
+    strncpy(*octet,ipStr,count);
+
+    *(*octet+count)='\0';
+
+    return (*p?++p:NULL);
+}
